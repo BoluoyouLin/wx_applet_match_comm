b0a639510508c0c7a825502fc8f8131a79560b96
ChenPrince <1163005753@qq.com>
2019-05-23 01:56:17 +0800

    新增分析结果页图片选择以及分析逻辑

--- miniprogram/pages/sharing/analyzeResult/analyzeResult.js
+++ miniprogram/pages/sharing/analyzeResult/analyzeResult.js
@@ -2,6 +2,7 @@
 const sourceType = [['camera'], ['album'], ['camera', 'album']]
 const sizeType = [['compressed'], ['original'], ['compressed', 'original']]
 let flag_analyzed = 0;
+
 Page({
 
   /**
@@ -17,7 +18,8 @@ Page({
     sizeTypeIndex: 2,
     sizeType: ['压缩', '原图', '压缩或原图'],
     countIndex: 8,
-    count: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    count: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    flag_analyzed:0
   },
 
   sourceTypeChange(e) {
@@ -36,21 +38,91 @@ Page({
     })
   },
 
-  chooseImage() {
+  // 选择发布图片
+  chooseImage:function(){
+    console.log("===> chooseImage")
     const that = this;
+    wx.chooseImage({
 
+      sourceType: sourceType[this.data.sourceTypeIndex], //图片来源
+      sizeType: sizeType[this.data.sizeTypeIndex], //图片类型
+      count: this.data.count[this.data.countIndex], //最大选择数量
+
+      success: res => {
+        let box =that.data.imageList //数组
+
+        let temp=res.tempFilePaths
+
+        console.log("--->before",box)
+
+        for(let i=0;i<temp.length;i++){
+            box.push(temp[i])
+        }
+        console.log("--->after",box)
+        that.setData({
+          imageList: box
+        })
+   
+      },
+
+      fail:err=>{
+        console.error(err)
+      }
+
+    })
+
+  },
+
+  // 选择分析图片
+  chooseAnalyzeImage() {
+    const that = this;
+    console.log("===> chooseAnalyzeImage")
     wx.chooseImage({
       sourceType: sourceType[this.data.sourceTypeIndex],
       sizeType: sizeType[this.data.sizeTypeIndex],
       count: this.data.count[this.data.countIndex],
       success: res => {
+
+        let one//只分析第一个
+        //放到数组第一位
+        let box=[]
+        let imageList=that.data.imageList
+
+        if(res.tempFilePaths.length>=1){
+          one=res.tempFilePaths[0]
+          box.push(one)
+        }else{
+          return
+        }
+        
+        console.log('one-->box',box)
+
+        //没有分析图
+        if(that.data.flag_analyzed==0){
+          console.log('0')
+          
+          for(let i=0;i<imageList.length;i++){
+            box.push(imageList[i])
+          }
+          this.setData({
+            flag_analyzed:1
+          })
+          // flag_analyzed=1;
+        }else{
+          console.log('1')
+          
+          //去掉首位
+          for(let i=1;i<imageList.length;i++){
+            box.push(imageList[i])
+          }
+        }
+
         that.setData({
-          imageList: res.tempFilePaths
+          imageList: box
         })
-        // 仅第一张图分析, 一次性选择多张图不分析
-        if (!flag_analyzed && that.data.imageList.length <= 1) {
+       
         wx.getFileSystemManager().readFile({
-          filePath: res.tempFilePaths[0], //选择图片返回的相对路径
+          filePath: one, //选择图片返回的相对路径
           encoding: 'base64', //编码格式
           success: res => { //成功的回调
             //console.log(res.data);
@@ -66,15 +138,15 @@ Page({
                 this.setData({
                   text: JSON.stringify(tokens.result) //方便真机调试，显示在界面上
                 })
-              })
-            }
-          })
-          flag_analyzed = 1;
-        }  //仅第一张图分析
+            })
+          }
+        })
+        
+      }  //仅第一张图分析
 
-        }
-      })
-   },
+      
+    })
+  },
 
   previewImage(e) {
     const current = e.target.dataset.src
@@ -89,46 +161,87 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    flag_analyzed = 0;
+    this.setData({
+      flag_analyzed:0
+    })
 
     //不知道怎么直接调用 chooseImage(), 先复制 chooseImage()的代码了
     const that = this;
+    that.chooseAnalyzeImage()
+    // wx.chooseImage({
+    //   sourceType: sourceType[this.data.sourceTypeIndex],
+    //   sizeType: sizeType[this.data.sizeTypeIndex],
+    //   count: this.data.count[this.data.countIndex],
+    //   success: res => {
+    //     that.setData({
+    //       imageList: res.tempFilePaths
+    //     })
 
-    wx.chooseImage({
-      sourceType: sourceType[this.data.sourceTypeIndex],
-      sizeType: sizeType[this.data.sizeTypeIndex],
-      count: this.data.count[this.data.countIndex],
-      success: res => {
-        that.setData({
-          imageList: res.tempFilePaths
-        })
-
-        // 仅第一张图分析, 一次性选择多张图不分析
-        if (!flag_analyzed && that.data.imageList.length <= 1) {
-          wx.getFileSystemManager().readFile({
-            filePath: res.tempFilePaths[0], //选择图片返回的相对路径
-            encoding: 'base64', //编码格式
-            success: res => { //成功的回调
-              //console.log(res.data);
-              wx.cloud.callFunction({
-                name: 'get_tokens',
-                data: {
-                  imgbase64: res.data  //调用识别API, base64不需要头部（默认就没有）
-                }
-              }).then(
-                (tokens) => {
-                  console.log(tokens.result);
-                  this.setData({
-                    text: JSON.stringify(tokens.result) //方便真机调试，显示在界面上
-                  })
-                })
-            }
-          })
-          flag_analyzed = 1;
-        }  //仅第一张图分析
-      }
-    })
+    //     // 仅第一张图分析, 一次性选择多张图不分析
+    //     if (!flag_analyzed && that.data.imageList.length <= 1) {
+    //       wx.getFileSystemManager().readFile({
+    //         filePath: res.tempFilePaths[0], //选择图片返回的相对路径
+    //         encoding: 'base64', //编码格式
+    //         success: res => { //成功的回调
+    //           //console.log(res.data);
+    //           wx.cloud.callFunction({
+    //             name: 'get_tokens',
+    //             data: {
+    //               imgbase64: res.data  //调用识别API, base64不需要头部（默认就没有）
+    //             }
+    //           }).then(
+    //             (tokens) => {
+    //               console.log(tokens.result);
+    //               this.setData({
+    //                 text: JSON.stringify(tokens.result) //方便真机调试，显示在界面上
+    //               })
+    //             })
+    //         }
+    //       })
+    //       flag_analyzed = 1;
+    //     }  //仅第一张图分析
+    //   }
+    // })
   },
+  // 删除图片
+  deleteImage: function (e) {
+    var that = this;
+    var imageList = that.data.imageList;
+    var index = e.currentTarget.dataset.index;//获取当前长按图片下标
+    console.log(index)
+    wx.showModal({
+     title: '提示',
+     content: '确定要删除此图片吗？',
+     success: function (res) {
+      if (res.confirm) {
+
+        console.log('点击确定了');
+        if(that.data.flag_analyzed == 1&&index==0){
+
+          // flag_analyzed=0;//并把之前的数据删掉
+          that.setData({
+            flag_analyzed:0
+          })
+          imageList.splice(index, 1);
+
+        }else{
+          imageList.splice(index, 1);
+        }
+
+        that.setData({
+          imageList:imageList
+        })
+        // images.splice(index, 1);
+
+      } else if (res.cancel) {
+
+        console.log('点击取消了');
+        return false;    
+      }
+      
+     }
+    })
+   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
